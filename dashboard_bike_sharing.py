6ec34ea0f453978fab844ead4e5bc915c2613fba
fannof <99071605+fannof@users.noreply.github.com>
2023-11-22 18:33:00 +0700

    Update dashboard_bike_sharing.py

--- dashboard_bike_sharing.py
+++ dashboard_bike_sharing.py
@@ -11,7 +11,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from babel.numbers import format_currency
 sns.set(style='dark')
 
 # Membaca dataset
